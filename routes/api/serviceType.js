d86bf10bcde239282f05944b22a193cef5f77e0c
ajaramillo <ajaramillo@eharmony.com>
2015-05-27 08:45:15 -0700

    Change environmental route name

--- routes/api/serviceType.js
+++
@@ -1,8 +0,0 @@
-var SvcType = require('../../models/ServiceType');
-
-exports.getAll = function(req, res, next) {
-  SvcType.find({active: true}, '_id name', function(err, svcType) {
-    if (err) return next(err);
-    res.json(svcType)
-  });
-};
