0853fe07e850b91a350a33221bd011fd69374b60
Daniel Silva <dniskav@gmail.com>
2015-06-02 21:16:00 -0500

    adde a ngid field in database

--- routes/api/servicetype.js
+++ routes/api/servicetype.js
@@ -1,7 +1,7 @@
 var SvcType = require('../../models/ServiceType');
 
 exports.getAll = function(req, res, next) {
-  SvcType.find({active: true}, '_id name', function(err, svcType) {
+  SvcType.find({active: true}, '_id name ngId', function(err, svcType) {
     if (err) return next(err);
     res.json(svcType)
   });
