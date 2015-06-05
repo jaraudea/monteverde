9caa0ec7af0b914fbcec34928e37a44b1121bd5e
Daniel Silva <dniskav@gmail.com>
2015-06-05 05:14:02 -0500

    first step in the typeahead to fill the form when the code exist, now we don't have: zone, active and an in the species table (specie and task)

--- src/modules/createSvc/createSvc.controller.js
+++ src/modules/createSvc/createSvc.controller.js
@@ -63,6 +63,15 @@ monteverde.controller('createSvcCtrl', function ($state, $scope, ngTableParams, 
         });
   }
 
+  $scope.getServiceConfig = function (_id) {
+    dataGet('serviceConf', _id, function (data) {
+      console.log(_id, data);
+      $scope.formData = data[0];
+      dataSpecieTable = data[0].treeSpeciesByTask;
+      $scope.tableParams.reload();
+    });
+  };
+
   var init = function () {
     dataGet('zones');
     dataGet('contracts');
@@ -71,13 +80,23 @@ monteverde.controller('createSvcCtrl', function ($state, $scope, ngTableParams, 
     dataGet('species');
     dataGet('tasks');
     dataGet('envAuths');
+    dataGet('codes', '', function (data) {
+      for (var ndx in data) {
+        $scope.codes.push(data[ndx].code);
+      };
+    });
   }
 
   // Method to GET data
-  var dataGet = function (type) {
-    connectorService.getData(connectorService.ep[type])
+  var dataGet = function (type, param, callback) {
+    var url = (typeof param !== 'undefined') ? connectorService.ep[type] + param : connectorService.ep[type];
+    connectorService.getData(url)
       .then(function (data) {
-        $scope.controls[type] = data;
+        if (typeof callback === 'function'){
+          callback(data);
+        } else {
+          $scope.controls[type] = data;
+        };
       });    
   }
 
