548397e9048f12df4d9cdb0baeda309799373cdc
Daniel Silva <dniskav@gmail.com>
2015-06-14 01:29:30 -0500

    setup a smooth method to close the alert

--- src/modules/common/alerts/alerts.factory.js
+++ src/modules/common/alerts/alerts.factory.js
@@ -1,17 +1,20 @@
 // Factory to manage the alerts
 'use strict';
 
-common.factory('AlertsFactory', function ($rootScope) {
+common.factory('AlertsFactory', function ($rootScope, $timeout) {
   var factory = {};
 
   $rootScope.alerts = [];
 
-  factory.addAlert = function(type, msg) {
+  factory.addAlert = function(type, msg, disposable) {
     $rootScope.alerts.push({type: type, msg: msg});
   };
 
-  factory.closeAlert = function(index) {
-    $rootScope.alerts.splice(index, 1);
+  factory.closeAlert = function(index, alert) {
+    alert.alert.destroy = "opacity : 0";
+    $timeout (function () {
+      $rootScope.alerts.splice(index, 1);
+    }, 500);
   }; 
 
   $rootScope.addAlert = factory.addAlert;
